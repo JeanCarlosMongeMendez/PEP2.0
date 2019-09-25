ccc48219781648d29e3da69ab227c4a9c96fc3e7
Leonardo Carrion <leo.carrion.23@gmail.com>
2019-09-25 12:49:04 -0600

    Pantalla de Cargas Sociales

---
+++ PEP2.0/Entidades/CargaSocial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    /// <summary>
+    /// Leonardo Carrion
+    /// 20/sep/2019
+    /// Clase para administrar la entidad de Carga Social
+    /// </summary>
+    public class CargaSocial
+    {
+        public int idCargaSocial { get; set; }
+        public String descCargaSocial { get; set; }
+        public Double porcentajeCargaSocial { get; set; }
+        public Boolean activo { get; set; }
+        public Partida partida { get; set; }
+        public Periodo periodo { get; set; }
+    }
+}
