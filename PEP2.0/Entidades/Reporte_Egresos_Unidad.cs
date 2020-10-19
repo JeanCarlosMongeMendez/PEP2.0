ffc18fe5ca75bf812792a2c667014b594a70fdeb
carrion91 <leo.carrion.23@gmail.com>
2020-10-19 14:27:05 -0600

    Reporte de Egresos por Unidad

---
+++ PEP2.0/Entidades/Reporte_Egresos_Unidad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Reporte_Egresos_Unidad
+    {
+        public String nombreUnidad { get; set; }
+        public String numeroPartida { get; set; }
+        public String descPartida { get; set; }
+        public String descPresupuesto { get; set; }
+        public Double monto { get; set; }
+    }
+}
